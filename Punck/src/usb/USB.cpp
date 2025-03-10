89541dc496ea8b67884785e6c6dc07414c4f987f
Dominic Webb <dwebb@reapit.com>
2025-03-10 15:19:19 +0000

    Fixed USB enumerating on Linux

--- Punck/src/usb/USB.cpp
+++ Punck/src/usb/USB.cpp
@@ -19,13 +19,13 @@ size_t _write(int handle, const unsigned char* buf, size_t bufSize)
 // procedure to allow classes to pass configuration data back via endpoint 1 (eg CDC line setup, MSC MaxLUN etc)
 void USB::EP0In(const uint8_t* buff, const uint32_t size)
 {
-	ep0.inBuffSize = size;
+	ep0.inBuffSize = std::min(size, static_cast<uint32_t>(req.Length));
 	ep0.inBuff = buff;
 	ep0State = EP0State::DataIn;
 
 	USBUpdateDbg({}, {}, {}, ep0.inBuffSize, {}, (uint32_t*)ep0.inBuff);
 
-	EPStartXfer(Direction::in, 0, size);		// sends blank request back
+	EPStartXfer(Direction::in, 0, ep0.inBuffSize);		// sends blank request back
 }
 
 
